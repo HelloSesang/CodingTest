d17e1ad0fab5c2488145328a3e26632b70637ae9
Depther <victory2373@gmail.com>
2019-07-25 20:30:52 +0900

    add GCD

--- src/AddOneToN.java
+++ src/AddOneToN.java
@@ -9,7 +9,6 @@ public class AddOneToN {
 		int n = Integer.parseInt(br.readLine());
 		int kind = n % 2 == 0 ? 0 : 1;
 		int sum = 0;
-
 		if (kind == 0) {
 			sum = (n + 1) * (n / 2);
 		} else {
---
+++ src/GCD.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class GCD {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+
+		int answer = getGCD(a, b);
+		System.out.println(answer);
+	}
+
+	private static int getGCD(int a, int b) {
+		if (b == 0) return a;
+		return getGCD(b, a % b);
+	}
+
+}
