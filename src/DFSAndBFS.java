8d36e8e5146f8ff17b46e9b4556beb011a9472ef
Depther <victory2373@gmail.com>
2019-08-02 01:13:45 +0900

    DFS BFS 수정

--- src/DFSAndBFS.java
+++ src/DFSAndBFS.java
@@ -4,72 +4,62 @@ import java.util.*;
 
 public class DFSAndBFS {
 
-	private static int vertex;
-
-	private static int edge;
-
-	private static int start;
-
 	private static List<Integer>[] graph;
 
-	private static boolean[] check;
+	private static boolean[] visitYn;
 
 	private static Deque<Integer> queue;
 
-	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		vertex = Integer.parseInt(st.nextToken());
-		edge = Integer.parseInt(st.nextToken());
-		start = Integer.parseInt(st.nextToken());
-		graph = new ArrayList[vertex + 1];
-		queue = new ArrayDeque<Integer>();
-		for (int i = 1; i < vertex + 1; i++) {
+		int vertexCnt = Integer.parseInt(st.nextToken());
+		int edgeCnt = Integer.parseInt(st.nextToken());
+		int startVertex = Integer.parseInt(st.nextToken());
+		graph = new ArrayList[vertexCnt + 1];
+		for (int i = 0; i < graph.length; i++)
 			graph[i] = new ArrayList<>();
-		}
-		setGraph();
-		check = new boolean[vertex + 1];
-		dfs(start);
-		System.out.println();
-		check = new boolean[vertex + 1];
-		bfs(start);
-	}
 
-	private static void setGraph() throws Exception{
-		for (int i = 1; i < edge + 1; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			int pos1 = Integer.parseInt(st.nextToken());
-			int pos2 = Integer.parseInt(st.nextToken());
-			graph[pos1].add(pos2);
-			graph[pos2].add(pos1);
+		for (int i = 0; i < edgeCnt; i++) {
+			st = new StringTokenizer(br.readLine());
+			int first = Integer.parseInt(st.nextToken());
+			int second = Integer.parseInt(st.nextToken());
+			graph[first].add(second);
+			graph[second].add(first);
 		}
-		for (int i = 1, len = graph.length; i < len; i++) {
+		for (int i = 1; i <= vertexCnt; i++)
 			Collections.sort(graph[i]);
+
+		visitYn = new boolean[vertexCnt + 1];
+		dfs(startVertex);
+		System.out.println();
+		visitYn = new boolean[vertexCnt + 1];
+		queue = new ArrayDeque<>();
+		bfs(startVertex);
+	}
+
+	private static void dfs(int now) {
+		System.out.print(now + " ");
+		visitYn[now] = true;
+		for (int i = 0, len = graph[now].size(); i < len; i++) {
+			int to = graph[now].get(i);
+			if (!visitYn[to]) dfs(to);
 		}
 	}
 
-	private static void dfs(int pos) throws Exception {
-		check[pos] = true;
-		System.out.print(pos + " ");
-		for (int i = 0, len = graph[pos].size(); i < len; i++) {
-			int to = graph[pos].get(i);
-			if (!check[to]) dfs(to);
-		}
-	}
+	private static void bfs(int now) {
+		visitYn[now] = true;
+		queue.offerLast(now);
+		System.out.print(now + " ");
 
-	private static void bfs(int pos) throws Exception {
-		check[pos] = true;
-		queue.offerLast(pos);
-
-		while(!queue.isEmpty()) {
-			int now = queue.pollFirst();
-			System.out.print(now + " ");
+		while(queue.size() > 0) {
+			now = queue.pollFirst();
 			for (int i = 0, len = graph[now].size(); i < len; i++) {
 				int to = graph[now].get(i);
-				if (!check[to]) {
+				if (!visitYn[to]) {
+					visitYn[to] = true;
 					queue.offerLast(to);
-					check[to] = true;
+					System.out.print(to + " ");
 				}
 			}
 		}
