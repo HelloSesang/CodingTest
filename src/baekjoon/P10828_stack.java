a4274fa9d3a46f71bbcc563e2eb248ac92c2753d
Depther <victory2373@gmail.com>
2019-08-02 23:14:16 +0900

    그래프 연결 요소 문제 해결

--- src/baekjoon/P10828_stack.java
+++ src/baekjoon/P10828_stack.java
@@ -3,7 +3,7 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class P10828 {
+public class P10828_stack {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
