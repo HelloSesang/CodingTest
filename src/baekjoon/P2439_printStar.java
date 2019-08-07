6459d15e46d5df742b982d105e2e4fabe5b3aa12
Depther <victory2373@gmail.com>
2019-08-07 11:37:46 +0900

    스택 관련 문제 풀이

---
+++ src/baekjoon/P2439_printStar.java
@@ -0,0 +1,24 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P2439_printStar {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int count = Integer.parseInt(br.readLine());
+		for (int i = 0; i < count; i++) {
+			StringBuilder sb = new StringBuilder();
+			for(int j = 0; j < count; j++) {
+				if (j + i + 1 >= count) {
+					sb.append("*");
+				} else {
+					sb.append(" ");
+				}
+			}
+			System.out.println(sb);
+		}
+	}
+
+}
---
+++ src/baekjoon/P9012_stack.java
@@ -0,0 +1,38 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class P9012_stack {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int count = Integer.parseInt(br.readLine());
+		while (count-- > 0) {
+			String input = br.readLine();
+			Deque<Character> stack = new ArrayDeque();
+			boolean result = true;
+			int len = input.length();
+			for (int i = 0; i < len; i++) {
+				if (!result) break;
+				switch(input.charAt(i)) {
+					case '(':
+						stack.offerLast('(');
+						break;
+					case ')':
+						if (stack.pollLast() != null || stack.pollLast() != '(')
+							result = false;
+						break;
+				}
+			}
+			if (stack.size() > 0 || result == false) {
+				System.out.println("NO");
+			} else {
+				System.out.println("YES");
+			}
+		}
+	}
+
+}
