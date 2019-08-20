e1543a552f546cd520b92056d517fcf31e30c359
Depther <victory2373@gmail.com>
2019-08-20 12:50:02 +0900

    acm-icpc GDC 합 구하는 문제 풀이

---
+++ src/main/java/acm_icpc/year2013/sumOfGCD.java
@@ -0,0 +1,32 @@
+package acm_icpc.year2013;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class sumOfGCD {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int testCnt = Integer.parseInt(br.readLine());
+		while(testCnt-- > 0) {
+			long answer = 0;
+			long[] input = Arrays.stream(br.readLine().split(" "))
+					             .mapToLong(Long::parseLong)
+								 .toArray();
+			for (int i = 1, len = input.length; i < len; i++) {
+				for (int j = i + 1; j < len; j++) {
+					answer += getGCD(input[i], input[j]);
+				}
+			}
+			System.out.println(answer);
+		}
+
+	}
+
+	private static long getGCD(long num1, long num2) {
+		if (num2 == 0) return num1;
+		return getGCD(num2, num1 % num2);
+	}
+
+}
