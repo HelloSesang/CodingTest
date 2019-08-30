f82641988c79e8f7bc6688a5cb95c77ac6863c96
Depther <victory2373@gmail.com>
2019-08-30 22:56:42 +0900

    dp, 스택 문제풀이

---
+++ src/main/java/baekjoon/P11727_dp.java
@@ -0,0 +1,31 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P11727_dp {
+
+	private static int[] dp;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int size = Integer.parseInt(br.readLine());
+		dp = new int[size + 1];
+		System.out.println(getAnswer(size));
+	}
+
+	private static int getAnswer(int size) {
+		if (size == 1) {
+			return 1;
+		}
+		if (size == 2) {
+			return 3;
+		}
+		if (dp[size] > 0) {
+			return dp[size];
+		}
+		dp[size] = (getAnswer(size - 1) % 10007 + (getAnswer(size - 2) * 2) % 10007) % 10007;
+		return dp[size];
+	}
+
+}
