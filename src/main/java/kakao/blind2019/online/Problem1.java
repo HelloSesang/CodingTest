e17a022e1a8ba32685bc7283413d15a17c418dd4
Depther <victory2373@gmail.com>
2019-09-09 15:05:58 +0900

    2019년도 카카오 공채 1차 1번 문제 주석 추가

--- src/main/java/kakao/blind2019/online/Problem1.java
+++ src/main/java/kakao/blind2019/online/Problem1.java
@@ -1,6 +1,3 @@
-// 압축할 문자열 s가 매개변수로 주어질 때, 위에 설명한 방법으로 1개 이상 단위로 문자열을 잘라 압축하여
-// 표현한 문자열 중 가장 짧은 것의 길이를 return 하도록 solution 함수를 완성해주세요.
-
 package kakao.blind2019.online;
 
 public class Problem1 {
@@ -10,11 +7,16 @@ public class Problem1 {
 		System.out.println(solution.solution("xababcdcdababcdcd"));
 	}
 
+	// 압축할 문자열 s가 매개변수로 주어질 때, 위에 설명한 방법으로 1개 이상 단위로 문자열을 잘라 압축하여
+	// 표현한 문자열 중 가장 짧은 것의 길이를 return 하도록 solution 함수를 완성해주세요.
 	public int solution(String s) {
+		// 문자열 길이가 1이하인 경우 바로 처리
 		if (s.length() <= 1) {
 			return s.length();
 		}
 
+		// getCompressedStr 메소드를 이용해서 압축된 문자열을 받아서
+		// 그 중 가장 짧은 길이 값을 저장했다가 반환
 		int answer = 0;
 		for (int i = 1, len = s.length() / 2; i <= len; i++) {
 			String result = getCompressedStr(s, i);
@@ -25,16 +27,22 @@ public class Problem1 {
 		return answer;
 	}
 
+	// 문자열을 압축하는 메소드
 	private String getCompressedStr(String str, int unit) {
+		// 압축한 문자열을 담을 StringBuilder 객체
 		StringBuilder compressedStr = new StringBuilder();
 
+		// 초기값 설정
 		String now = "";
 		int repeatCount = 0;
+
+		// 문자열에 순차접근하면서 압축
 		for (int i = 0, len = str.length(); i < len; i += unit) {
 			now = str.substring(i, i + unit);
 			repeatCount++;
 
 			String next = "";
+			// 다음에 접근할 부분이 끝인 경우
 			if (i + (unit * 2) > len) {
 				next = str.substring(i + unit);
 				if (now.equals(next)) {
@@ -47,6 +55,7 @@ public class Problem1 {
 					compressedStr.append(now).append(next);
 				}
 				break;
+			// 다음에 접근할 부분이 끝이 아닌 경우
 			} else {
 				next = str.substring(i + unit, i + (unit * 2));
 				if (!now.equals(next)) {
