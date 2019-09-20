a01fbf88b934b8361264d1efa9899305af24eb55
Depther <victory2373@gmail.com>
2019-09-20 15:14:19 +0900

    프로그래머스 자릿수 더하기 문제 풀이

---
+++ src/main/java/programmers/자릿수_더하기.java
@@ -0,0 +1,22 @@
+package programmers;
+
+public class 자릿수_더하기 {
+
+	public static void main(String[] args) {
+		자릿수_더하기 obj = new 자릿수_더하기();
+		int answer = obj.solution(123);
+		System.out.println(answer);
+	}
+
+	public int solution(int n) {
+		int answer = 0;
+
+		while (n != 0) {
+			answer += n % 10;
+			n /= 10;
+		}
+
+		return answer;
+	}
+
+}
