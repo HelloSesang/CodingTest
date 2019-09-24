5867814b0477f33f86e318b3a5aba0bcabddbc55
Depther <victory2373@gmail.com>
2019-09-24 11:09:06 +0900

    프로그래머스 정수 내림차순으로 배치하기 문제 풀이

---
+++ src/main/java/programmers/정수_내림차순으로_배치하기.java
@@ -0,0 +1,25 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class 정수_내림차순으로_배치하기 {
+
+	public long solution(long n) {
+		List<Integer> list = new ArrayList<>();
+
+		while (n != 0) {
+			list.add((int) (n % 10));
+			n /= 10;
+		}
+		Collections.sort(list, (o1, o2) -> o2 - o1);
+		StringBuilder sb = new StringBuilder();
+		for (int e : list) {
+			sb.append(e);
+		}
+
+		return Long.parseLong(sb.toString());
+	}
+
+}
---
+++ src/test/java/programmers/정수_내림차순으로_배치하기Test.java
@@ -0,0 +1,14 @@
+package programmers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class 정수_내림차순으로_배치하기Test {
+
+	@Test
+	public void solution() {
+		정수_내림차순으로_배치하기 obj = new 정수_내림차순으로_배치하기();
+		assertEquals(873211, obj.solution(118372));
+	}
+}
