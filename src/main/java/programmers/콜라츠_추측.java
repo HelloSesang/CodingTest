14e86ee9a9471c3aa045914e28ed2544f8875719
Depther <victory2373@gmail.com>
2019-09-25 14:48:54 +0900

    프로그래머스 콜라츠 추측 문제 풀이

---
+++ src/main/java/programmers/콜라츠_추측.java
@@ -0,0 +1,27 @@
+package programmers;
+
+public class 콜라츠_추측 {
+
+	public int solution(int num) {
+		return getRepeatCount(num, 0);
+	}
+
+	private int getRepeatCount(long num, int nowCount) {
+		if (nowCount >= 500) {
+			return -1;
+		}
+
+		if (num == 1) {
+			return nowCount;
+		}
+
+		if (num % 2 == 0) {
+			num /= 2;
+		} else {
+			num = num * 3 + 1;
+		}
+
+		return getRepeatCount(num, nowCount + 1);
+	}
+
+}
---
+++ src/test/java/programmers/콜라츠_추측Test.java
@@ -0,0 +1,14 @@
+package programmers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class 콜라츠_추측Test {
+
+	@Test
+	public void solution() {
+		콜라츠_추측 obj = new 콜라츠_추측();
+		assertEquals(-1, obj.solution(626331));
+	}
+}
