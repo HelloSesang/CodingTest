6892952def2eb747bd8895672737eb4d7d1d8c8b
Depther <victory2373@gmail.com>
2019-09-25 15:52:59 +0900

    프로그래머스 x만큼 간격이 있는 n개의 숫자 문제 풀이

---
+++ src/main/java/programmers/x만큼_간격이_있는_n개의_숫자.java
@@ -0,0 +1,19 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class x만큼_간격이_있는_n개의_숫자 {
+
+	public long[] solution(long x, int n) {
+		long[] answer = new long[n];
+
+		for (int i = 0; i < n; i++) {
+			answer[i] = x + (x * i);
+		}
+
+		System.out.println(Arrays.toString(answer));
+
+		return answer;
+	}
+
+}
---
+++ src/test/java/programmers/x만큼_간격이_있는_n개의_숫자Test.java
@@ -0,0 +1,14 @@
+package programmers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class x만큼_간격이_있는_n개의_숫자Test {
+
+	@Test
+	public void solution() {
+		x만큼_간격이_있는_n개의_숫자 obj = new x만큼_간격이_있는_n개의_숫자();
+		assertArrayEquals(new long[]{2,4,6,8,10}, obj.solution(2, 5));
+	}
+}
