771c01973c1fbfa261a8895428a91e74d3d0e287
Depther <victory2373@gmail.com>
2019-09-26 18:58:00 +0900

    프로그래머스 124 나라의 숫자 문제 풀이

---
+++ src/test/java/programmers/P124_나라의_숫자Test.java
@@ -0,0 +1,14 @@
+package programmers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class P124_나라의_숫자Test {
+
+	@Test
+	public void solution() {
+		P124_나라의_숫자 obj = new P124_나라의_숫자();
+		assertEquals("41", obj.solution(10));
+	}
+}
