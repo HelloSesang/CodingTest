baea94dd1d8a760f28b835fc392bff2d36d7c1e8
Depther <victory2373@gmail.com>
2019-10-11 00:22:22 +0900

    백준 1260 dfs, bfs 문제 풀이

---
+++ src/main/java/baekjoon/P1260_DFS와_BFS.java
@@ -0,0 +1,95 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class P1260_DFS와_BFS {
+
+	private static boolean[] check;
+	private static Deque<Integer> queue;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = br.readLine().split(" ");
+		// 정점 갯수
+		int vertexCount = Integer.parseInt(input[0]);
+		// 간선 갯수
+		int edgeCount = Integer.parseInt(input[1]);
+		// 검색 시작 정점
+		int startVertex = Integer.parseInt(input[2]);
+
+		// 그래프 자료구조 생성
+		List<Integer>[] graph = new ArrayList[vertexCount + 1];
+		for (int i = 0, len = graph.length; i < len; i++) {
+			graph[i] = new ArrayList<>();
+		}
+
+		// 그래프에 간선 정보 저장
+		while (edgeCount-- > 0) {
+			String[] edge = br.readLine().split(" ");
+			int point1 = Integer.parseInt(edge[0]);
+			int point2 = Integer.parseInt(edge[1]);
+			graph[point1].add(point2);
+			graph[point2].add(point1);
+		}
+
+		for (List list : graph) {
+			list.sort((Comparator) (o1, o2) -> (Integer)o1 - (Integer)o2);
+		}
+
+		check = new boolean[vertexCount + 1];
+		dfs(graph, startVertex);
+
+		System.out.println();
+
+		check = new boolean[vertexCount + 1];
+		queue = new ArrayDeque<>();
+		bfs(graph, startVertex);
+	}
+
+	public static void dfs(List<Integer>[] graph, int nowVertex) {
+		// 현재 정점에 check
+		check[nowVertex] = true;
+		// 정점을 출력
+		System.out.print(nowVertex + " ");
+		// 연결된 간선에 순차접근
+		for (int e : graph[nowVertex]) {
+			// check가 안된 정점인 경우
+			if (!check[e]) {
+				// 해당 정점으로 이동
+				dfs(graph, e);
+			}
+		}
+
+		// 모든 연결 정점을 다 돈 경우
+		return;
+	}
+
+	private static void bfs(List<Integer>[] graph, int nowVertex) {
+		// 처음 시작 정점 처리
+		if (!check[nowVertex]) {
+			check[nowVertex] = true;
+			System.out.print(nowVertex + " ");
+		}
+
+		// 연결된 정점에 순차 접근
+		for (int e : graph[nowVertex]) {
+			// 연결된 정점에 처리되지 않은 경우
+			if (!check[e]) {
+				// 연결된 정점 체크
+				check[e] = true;
+				// 출력
+				System.out.print(e + " ");
+				// 큐에 다음 정점 저장
+				queue.add(e);
+			}
+		}
+
+		// 큐가 비게될 때까지 다음 정점에 대해 bfs 처리
+		while(!queue.isEmpty()) {
+			bfs(graph, queue.pollFirst());
+		}
+	}
+
+}
---
+++ src/main/java/baekjoon/P14502_연구소.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+/*
+	바이러스가 퍼지는 것을 막기위해 연구소에 벽을 세운다.
+	연구소는 n*m의 직사각형
+	연구소는 빈 칸과 벽으로 구성
+	바이러스는 상하좌우 인접한 칸으로 퍼질 수있다.
+	세울 수 있는 벽은 3개이고 3개 다 세워야한다.
+	0은 빈 칸, 1은 벽, 2는 바이러스
+	벽을 세운 후 안전 영역 크기의 최댓값을 구하라.
+*/
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class P14502_연구소 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
