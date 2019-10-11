82a4a86219863006e676f09b0cf36ca1590e8712
Depther <victory2373@gmail.com>
2019-10-11 11:41:34 +0900

    st사 2번 문제 풀이

--- src/main/java/real/stcompany/Problem2_retry.java
+++ src/main/java/real/stcompany/Problem2_retry.java
@@ -9,22 +9,78 @@ package real.stcompany;
 	동일 digram이 존재하지 않으면  -1을 반환하라
 */
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem2_retry {
 
 	public int solution(String S) {
 		// answer 변수를 만들고 -1을 할당한다.
 		int answer = -1;
 
-		// 반복문을 이용해서 가장 왼쪽의 digram부터 순차접근한다.
-			// 내부 반복문을 이용해서 가장 오른쪽의 digram부터 순차접근한다.(내부 반복문의 index 값은 외부 반복문의 index보다는 커야한다.)
-				// 두 digram이 같은 경우
-					// 두 digram 사이의 거리가 answer의 값보다 큰 경우 answer에 둘 사이의 거리를 대입한다.
-					// break로 현재 digram에서 벗어난다.
-				// 두 digram이 다른 경우
-					// 한 칸 더 오른쪽의 digram에 접근한다.
+		// digram 정보를 보관할 map 자료구조
+		Map<String, DigramInfo> map = new HashMap<>();
+
+		// 반복문을 이용해서 모든 digram에 순차 접근한다.
+		for (int i = 1, len = S.length(); i < len; i++) {
+			// 현재 접근한 digram 변수
+			String nowDigram = S.substring(i - 1, i + 1);
+
+			// Digram 객체 찾기
+			DigramInfo digramInfo = map.getOrDefault(nowDigram, null);
+
+			// 처음 접하는 digram인 경우
+			if (digramInfo == null) {
+				// 객체와 start 값을 설정하고 map에 저장
+				map.put(nowDigram, new DigramInfo(i - 1, -1));
+			// 처음 접하는 digram이 아닌 경우
+			} else {
+				// map에 저장된 객체의 length 값을 설정
+				digramInfo.setLength((i - 1) - digramInfo.getStart());
+			}
+		}
+
+		// 반복문을 통해 map에 저장된 모든 value에 접근한다.
+		for (String s : map.keySet()) {
+			DigramInfo digramInfo = map.get(s);
+
+			// length 값이 answer보다 큰 경우
+			if (digramInfo.getLength() > answer) {
+				// answer에 length를 대입한다.
+				answer = digramInfo.getLength();
+			}
+		}
 
 		// 최종 결과값을 반환한다.
 		return answer;
+	}
+
+	class DigramInfo {
+
+		private int start;
+
+		private int length;
+
+		public DigramInfo(int start, int length) {
+			this.start = start;
+			this.length = length;
+		}
+
+		public int getStart() {
+			return start;
+		}
+
+		public void setStart(int start) {
+			this.start = start;
+		}
+
+		public int getLength() {
+			return length;
+		}
+
+		public void setLength(int length) {
+			this.length = length;
+		}
 	}
 
 }
