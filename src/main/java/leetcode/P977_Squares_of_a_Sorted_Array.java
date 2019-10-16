e867bd87e2ffa0cc6bcc2a09d681cb0848d90537
Depther <victory2373@gmail.com>
2019-10-16 09:06:23 +0900

    리트코드 977번 문제 풀이

---
+++ src/main/java/leetcode/P977_Squares_of_a_Sorted_Array.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.function.IntUnaryOperator;
+
+public class P977_Squares_of_a_Sorted_Array {
+
+	public int[] sortedSquares(int[] A) {
+		int[] result = Arrays.stream(A).map(operand -> operand * operand).toArray();
+		Arrays.sort(result);
+
+		return result;
+	}
+
+}
