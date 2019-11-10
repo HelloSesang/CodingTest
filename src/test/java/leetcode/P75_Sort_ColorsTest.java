db90174d72b258748e926c133bfddbf79befdd13
Depther <victory2373@gmail.com>
2019-11-10 15:08:04 +0900

    feat : Solve problem 75. sort colors

---
+++ src/test/java/leetcode/P75_Sort_ColorsTest.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+import org.junit.Test;
+
+public class P75_Sort_ColorsTest {
+
+	@Test
+	public void sortColors() {
+		P75_Sort_Colors obj = new P75_Sort_Colors();
+		obj.sortColors(new int[] {2,0,2,1,1,0});
+	}
+}
