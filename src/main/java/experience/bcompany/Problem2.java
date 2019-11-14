475bd7515dc026150b7fa2d6f95297497046fe83
Depther <victory2373@gmail.com>
2019-11-14 13:55:05 +0900

    refactor : Fix method architecture

--- src/main/java/experience/bcompany/Problem2.java
+++ src/main/java/experience/bcompany/Problem2.java
@@ -2,13 +2,6 @@ package experience.bcompany;
 
 public class Problem2 {
 
-	/*
-	 * Complete the 'programmerStrings' function below.
-	 *
-	 * The function is expected to return an INTEGER.
-	 * The function accepts STRING s as parameter.
-	 */
-
 	public static int programmerStrings(String s) {
 		if (s == null) {
 			throw new IllegalArgumentException();
@@ -16,86 +9,73 @@ public class Problem2 {
 
 		int leftEndIndex = findLeftProgrammerStringEndIndex(s);
 		int rightStartIndex = findRightProgrammerStringStartIndex(s);
+
 		int betweenCharCount = rightStartIndex - leftEndIndex - 1;
+
 		if (betweenCharCount < 0) {
 			return 0;
 		}
+
 		return betweenCharCount;
 	}
 
 	static int findLeftProgrammerStringEndIndex(String s) {
 		Condition condition = new Condition();
+
 		for (int i = 0, len = s.length(); i < len; i++) {
 			if (condition.isSatisfy(s.charAt(i))) {
 				return i;
 			}
 		}
+
 		return 0;
 	}
 
 	static int findRightProgrammerStringStartIndex(String s) {
 		Condition condition = new Condition();
+
 		for (int i = s.length() - 1; i >= 0; i--) {
 			if (condition.isSatisfy(s.charAt(i))) {
 				return i;
 			}
 		}
+
 		return 0;
 	}
 
 	static class Condition {
 
-		private int pCount;
-		private int rCount;
-		private int oCount;
-		private int gCount;
-		private int aCount;
-		private int mCount;
-		private int eCount;
-		private int totalCount;
-
-		public Condition() {
-			pCount = 1;
-			rCount = 3;
-			oCount = 1;
-			gCount = 1;
-			aCount = 1;
-			mCount = 2;
-			eCount = 1;
-			totalCount = 10;
-		}
-
+		private int pCount = 1;
+		private int rCount = 3;
+		private int oCount = 1;
+		private int gCount = 1;
+		private int aCount = 1;
+		private int mCount = 2;
+		private int eCount = 1;
+		private int totalCount = 10;
 
 		public boolean isSatisfy(char ch) {
-			switch (ch) {
-				case 'p' :
-					pCount--;
-					totalCount--;
-					break;
-				case 'r' :
-					rCount--;
-					totalCount--;
-					break;
-				case 'o' :
-					oCount--;
-					totalCount--;
-					break;
-				case 'g' :
-					gCount--;
-					totalCount--;
-					break;
-				case 'a' :
-					aCount--;
-					totalCount--;
-					break;
-				case 'm' :
-					mCount--;
-					totalCount--;
-					break;
-				case 'e' :
-					eCount--;
-					totalCount--;
-					break;
+			if (ch == 'p' && pCount > 0) {
+				pCount--;
+				totalCount--;
+			} else if (ch == 'r' && rCount > 0) {
+				rCount--;
+				totalCount--;
+			} else if (ch == 'o' && oCount > 0) {
+				oCount--;
+				totalCount--;
+			} else if (ch == 'g' && gCount > 0) {
+				gCount--;
+				totalCount--;
+			} else if (ch == 'a' && aCount > 0) {
+				aCount--;
+				totalCount--;
+			} else if (ch == 'm' && mCount > 0) {
+				mCount--;
+				totalCount--;
+			} else if (ch == 'e' && eCount > 0) {
+				eCount--;
+				totalCount--;
 			}
 
 			return totalCount <= 0;
