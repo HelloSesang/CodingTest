14c835cb6db4af1d18bbee179288934cbb021691
kyumin <kumin2373@gmail.com>
2020-04-04 11:22:34 +0900

    리트코드 7번 문제 풀이

---
+++ src/main/golang/leetcode/P7_Reverse_Integer.go
@@ -0,0 +1,18 @@
+package leetcode
+
+import (
+	"math"
+)
+
+func reverse(x int) int {
+	result := 0
+	for ; x != 0; x /= 10 {
+		result = (result * 10) + (x % 10)
+	}
+
+	if result > int(math.Pow(2, 31) - 1) || result < int(-math.Pow(2, 31))  {
+		result = 0
+	}
+
+	return result
+}
