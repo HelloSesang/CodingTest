448b62f34ea6e6ffa0d0585da5ce97873fc38ada
kyumin2373 <kyumin2373@gmail.com>
2020-04-22 17:27:40 +0900

    리트코드 26번 문제 풀이

---
+++ src/main/golang/leetcode/P26_Remove_Duplicates_from_Sorted_Array.go
@@ -0,0 +1,36 @@
+package leetcode
+
+import (
+	"fmt"
+	"sort"
+)
+
+type Asc []int
+
+func(a Asc) Len() int {
+	return len(a)
+}
+
+func(a Asc) Less(i, j int) bool {
+	return a[i] < a[j]
+}
+
+func(a Asc) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func main() {
+	fmt.Println(removeDuplicates([]int{2,2,2}))
+}
+
+func removeDuplicates(nums []int) int {
+	sort.Sort(Asc(nums))
+
+	for i := len(nums) - 1; i > 0 ; i-- {
+		if nums[i - 1] == nums[i] {
+			nums = append(nums[:i], nums[i+1:]...)
+		}
+	}
+
+	return len(nums)
+}
