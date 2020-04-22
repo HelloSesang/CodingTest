4a9d2ab453dfd2067f94b7663db6ceadf9dc0790
kyumin2373 <kyumin2373@gmail.com>
2020-04-22 17:42:03 +0900

    리트코드 27번 문제 풀이

---
+++ src/main/golang/leetcode/P27_Remove_Element.go
@@ -0,0 +1,16 @@
+package leetcode
+
+import "fmt"
+
+func main() {
+	fmt.Println(removeElement([]int{0,1,2,2,3,0,4,2}, 2))
+}
+
+func removeElement(nums []int, val int) int {
+	for i := len(nums) - 1; i >= 0 ; i-- {
+		if nums[i] == val {
+			nums = append(nums[:i], nums[i + 1:]...)
+		}
+	}
+	return len(nums)
+}
