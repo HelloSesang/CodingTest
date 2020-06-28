9adece4de8569475e81f6655f5e35858cccfd99e
Depther <victory2373@gmail.com>
2020-06-28 17:30:45 +0900

    solve leetcode problem 28

--- src/main/golang/main.go
+++ src/main/golang/main.go
@@ -1,23 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
-
-func maxProfit(prices []int) int {
-	min := math.MaxInt64
-	max := 0
-	for i := 0; i < len(prices); i++ {
-		if min > prices[i] {
-			min = prices[i]
-		} else if prices[i] - min > max {
-			max = prices[i] - min
-		}
-	}
-	return max
-}
+import "strings"
 
 func main() {
-	fmt.Println(maxProfit([]int{7,1,5,3,6,4}))
+
+}
+
+func strStr(haystack string, needle string) int {
+	strings.Index(haystack, needle)
 }
--- src/main/java/leetcode/P28_Implement_strStr.java
+++ src/main/java/leetcode/P28_Implement_strStr.java
@@ -1,4 +1,6 @@
-class Solution {
+package leetcode;
+
+public class P28_Implement_strStr {
     public int strStr(String haystack, String needle) {
         return haystack.indexOf(needle);
     }
