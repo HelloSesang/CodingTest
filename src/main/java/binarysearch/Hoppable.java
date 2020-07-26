683e749964a678f532bd2dc5cfa3b3cfe7742e5c
Depther <victory2373@gmail.com>
2020-07-26 09:33:45 +0900

    refactor: improve problem binarysearch > Hoppable using greedy algorithm

--- src/main/java/binarysearch/Hoppable.java
+++ src/main/java/binarysearch/Hoppable.java
@@ -2,12 +2,35 @@ package binarysearch;
 
 public class Hoppable {
 
-    public boolean solve(int[] nums) {
+    /**
+     * 첫 번째 해결 방법
+     * 특징: 재귀, 2중 For문
+     */
+    public boolean solve1(int[] nums) {
         return recursive(0, nums);
     }
 
+    /**
+     * 개선된 해결 방법
+     * 특징: Greedy 알고리즘
+     * 상세: 첫 번째 해결 방법에서 2번째 For문을 최선의 경우만 확인하는 Greedy 알고리즘 적용으로 제거
+     */
+    public boolean solve(int[] nums) {
+        int goal = nums.length - 1;
+        int reachable = 0;
+
+        for (int i = 0; i <= reachable; i++) {
+            reachable = Math.max(reachable, i + nums[i]);
+
+            if (reachable >= goal) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     private boolean recursive(int index, int[] nums) {
-        if (nums.length - 1 <= index) {
+        if (nums.length - 1 <= index + nums[index]) {
             return true;
         }
 
