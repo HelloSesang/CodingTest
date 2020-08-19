da56bc77bd621af4ccef2145c726e257eb311585
Depther <victory2373@gmail.com>
2020-08-19 10:27:23 +0900

    feat: solve problem leetcode 496 using golang

--- src/main/golang/leetcode/P496_Next_Greater_Element_I.go
+++ src/main/golang/leetcode/P496_Next_Greater_Element_I.go
@@ -1,13 +1,6 @@
-package main
+package leetcode
 
 import "fmt"
-
-func main() {
-	nums1 := []int {4,1,2}
-	nums2 := []int {1,3,4,2}
-
-	fmt.Println(nextGreaterElement(nums1, nums2))
-}
 
 func nextGreaterElement(nums1 []int, nums2 []int) []int {
 	result := make([]int, len(nums1))
