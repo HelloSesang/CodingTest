dd9c4fb5488af9277cb30b7d84d48d105df758fd
Depther <victory2373@gmail.com>
2020-11-16 22:23:48 +0900

    feat: solve problem leetcode 344 using java

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,9 +1,28 @@
-import java.util.List;
+import java.util.Arrays;
 
 public class Solution {
 
 	public static void main(String[] args) {
+		Solution solution = new Solution();
+		char[] s = {'h', 'e', 'l', 'l', 'o'};
+		solution.reverseString(s);
+	}
 
+	public void reverseString(char[] s) {
+		int left = 0;
+		int right = s.length - 1;
+
+		char temp = ' ';
+		while (left <= right) {
+			temp = s[left];
+			s[left] = s[right];
+			s[right] = temp;
+
+			left++;
+			right--;
+		}
+
+		System.out.println(Arrays.toString(s));
 	}
 
 }
