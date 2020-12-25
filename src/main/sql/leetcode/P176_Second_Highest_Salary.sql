781964a7ea96c6cdfa15bbe247f7bfffc4de41db
kyumin <kyumin.choi@nhntoast.com>
2020-12-25 15:02:19 +0900

    feat: solve problem leetcode 176 using sql

---
+++ src/main/sql/leetcode/P176_Second_Highest_Salary.sql
@@ -0,0 +1,7 @@
+SELECT (CASE WHEN (SELECT COUNT(DISTINCT Salary)
+                     FROM Employee) < 2 THEN NULL
+             ELSE (SELECT Salary
+                     FROM Employee
+                    ORDER BY Salary DESC
+                    LIMIT 1,1)
+        END) AS SecondHighestSalary
