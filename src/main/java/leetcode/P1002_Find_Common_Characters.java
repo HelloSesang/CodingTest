86552eefe19bea70ecbd3a3cc77201c588802920
kyumin <kyumin.choi@nhntoast.com>
2020-12-27 15:49:46 +0900

    refactor: refactor problem leetcode 1002 using java

--- src/main/java/leetcode/P1002_Find_Common_Characters.java
+++ src/main/java/leetcode/P1002_Find_Common_Characters.java
@@ -8,35 +8,40 @@ import java.util.Map;
 public class P1002_Find_Common_Characters {
 
     public List<String> commonChars(String[] A) {
-        List<String> result = new ArrayList<>();
+        List<String> answer = new ArrayList<>();
 
-        List<Map<Character, Integer>> list = new ArrayList<>();
-        for (int i = 0; i < A.length; i++) {
-            list.add(i, new HashMap<>());
+        // 공통으로 사용되는 문자의 갯수를 저장하는 배열
+        int[] dict = new int[26];
+
+        // 첫 번째 문자열에 나오는 모든 문자의 갯수를 세서 저장
+        for (int j = 0; j < A[0].length(); j++) {
+            dict[A[0].charAt(j) - 'a']++;
         }
 
-        for (int i = 0; i < A.length; i++) {
-            for (Character ch : A[i].toCharArray()) {
-                Map<Character, Integer> map = list.get(i);
-                int count = map.getOrDefault(ch, 0);
-                map.put(ch, count + 1);
+        // 첫 번째를 제외한 나머지 문자열에 순차접근
+        for (int i = 1; i < A.length; i++) {
+            // 현재 접근한 문자열에 존재하는 문자들의 갯수를 저장하는 배열
+            int[] curr = new int[26];
+
+            // 현재 접근한 문자열에 나오는 모든 문자의 갯수를 세서 저장
+            for (int j = 0; j < A[i].length(); j++) {
+                curr[A[i].charAt(j) - 'a']++;
+            }
+
+            // 공통으로 사용되는 문자의 갯수를 저장하는 배열에 저장된 문자 출현 갯수를 출현 횟수가 가장 적은 숫자로 업데이트한다.
+            for (int j = 0; j < 26; j++) {
+                if (curr[j] < dict[j]) dict[j] = curr[j];
             }
         }
 
-        Map<Character, Integer> map = list.get(0);
-        for (Character ch : map.keySet()) {
-            int count = map.get(ch);
-            for (int i = 1; i < list.size(); i++) {
-                if (list.get(i).getOrDefault(ch, 0) < count) {
-                    count = list.get(i).getOrDefault(ch, 0);
-                }
-            }
-            for (int i = 0; i < count; i++) {
-                result.add(ch.toString());
+        // 최종적으로 정리된 공통 문자 갯수를 체크해서 정답을 완성한다.
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < dict[i]; j++) {
+                answer.add(Character.toString((char) ('a' + i)));
             }
         }
 
-        return result;
+        return answer;
     }
 
 }
