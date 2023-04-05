37819e48c380451cbc9a93946ec145cc3cd09c8a
kyumin <kyumin.choi@nhnfashiongo.com>
2023-04-05 11:09:32 +0900

    solve inflearn algorithm problems number 3, 4

---
+++ src/main/java/inflearn/Problem3.java
@@ -0,0 +1,23 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class Problem3 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input1 = scanner.nextLine();
+        String[] words = input1.split(" ");
+        int index = 0;
+        int length = 0;
+
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].length() > length) {
+                index = i;
+                length = words[i].length();
+            }
+        }
+
+        System.out.println(words[index]);
+    }
+}
---
+++ src/main/java/inflearn/Problem4.java
@@ -0,0 +1,27 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class Problem4 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int wordCount = in.nextInt();
+        in.nextLine();
+        String[] words = new String[wordCount];
+
+        for (int i = 0; i < wordCount; i++) {
+            words[i] = in.nextLine();
+        }
+
+        for (int i = 0; i < wordCount; i++) {
+            char[] word = words[i].toCharArray();
+            for (int j = word.length - 1; j >= 0; j--) {
+                System.out.print(word[j]);
+            }
+            System.out.println();
+        }
+    }
+
+}
