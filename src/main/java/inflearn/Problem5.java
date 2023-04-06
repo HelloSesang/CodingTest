1175a64842690d863d72b45dc43855c85c6fb5e0
Depther <victory2373@gmail.com>
2023-04-06 22:46:24 +0900

    solve problems number 5, 6

---
+++ src/main/java/inflearn/Problem5.java
@@ -0,0 +1,33 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class Problem5 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+
+        char[] chars = input.toCharArray();
+        char temp;
+        int i = 0;
+        int j = chars.length - 1;
+
+        while (i < j && j > i) {
+            while (!Character.isLetter(chars[i])) {
+                i++;
+            }
+            while (!Character.isLetter(chars[j])) {
+                j--;
+            }
+            temp = chars[i];
+            chars[i] = chars[j];
+            chars[j] = temp;
+            i++;
+            j--;
+        }
+
+        System.out.println(String.valueOf(chars));
+    }
+
+}
---
+++ src/main/java/inflearn/Problem6.java
@@ -0,0 +1,29 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class Problem6 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+
+        StringBuilder result = new StringBuilder();
+
+        char[] chars = input.toCharArray();
+        boolean[] isDuplicated = new boolean[chars.length];
+        for (int i = 0; i < input.length(); i++) {
+            if (isDuplicated[i]) continue;
+            result.append(chars[i]);
+
+            for (int j = i + 1; j < input.length(); j++) {
+                char base = chars[i];
+                if (base == chars[j]) {
+                    isDuplicated[j] = true;
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+}
