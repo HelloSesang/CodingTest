8f7453f3cf4af69d499330eb83e8c10a20842c81
kyumin <victory2373@gmail.com>
2023-04-12 17:34:36 +0900

    solve problem number 11, 12

---
+++ src/main/java/inflearn/Problem11.java
@@ -0,0 +1,55 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class Problem11 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+
+        StringBuilder result = new StringBuilder();
+        int repeatCount = 0;
+        char beforeCharacter = ' ';
+
+        for (int i = 0; i < input.length(); i++) {
+            char current = input.charAt(i);
+            if (isFirstCharacter(i)) {
+                beforeCharacter = current;
+                result.append(current);
+                repeatCount++;
+                continue;
+            }
+
+            if (isCharacterRepeated(beforeCharacter, current)) {
+                repeatCount++;
+                continue;
+            }
+
+            if (isThereRepeatHistory(repeatCount)) {
+                result.append(repeatCount);
+                repeatCount = 1;
+            }
+            result.append(current);
+            beforeCharacter = current;
+        }
+
+        if (isThereRepeatHistory(repeatCount)) {
+            result.append(repeatCount);
+        }
+
+        System.out.println(result);
+    }
+
+    private static boolean isThereRepeatHistory(int repeatCount) {
+        return repeatCount > 1;
+    }
+
+    private static boolean isCharacterRepeated(char beforeCharacter, char current) {
+        return current == beforeCharacter;
+    }
+
+    private static boolean isFirstCharacter(int i) {
+        return i == 0;
+    }
+}
---
+++ src/main/java/inflearn/Problem12.java
@@ -0,0 +1,47 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class Problem12 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int characterCount = in.nextInt();
+        in.nextLine();
+        String input = in.nextLine();
+
+        StringBuilder result = new StringBuilder();
+        while(characterCount-- > 0) {
+            String encryptedCharacter = input.substring(0, 7);
+            result.append(decryptCharacter(encryptedCharacter));
+            input = input.substring(7);
+        }
+        System.out.println(result);
+    }
+
+    private static char decryptCharacter(String encryptedCharacter) {
+        String binary = createBinary(encryptedCharacter);
+        int decimalNumber = createDecimalNumber(binary);
+        return createASCII(decimalNumber);
+    }
+
+    private static String createBinary(String encryptedCharacter) {
+        StringBuilder result = new StringBuilder();
+        for (char ch : encryptedCharacter.toCharArray()) {
+            if (ch == '#') {
+                result.append(1);
+            } else {
+                result.append(0);
+            }
+        }
+        return result.toString();
+    }
+
+    private static int createDecimalNumber(String binary) {
+        return Integer.parseInt(binary, 2);
+    }
+
+    private static char createASCII(int decimalNumber) {
+        return (char) decimalNumber;
+    }
+}
